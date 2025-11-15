63ecdb8bebb6aeb63e1d2f9e30715ff2ab145eec
0quepasariasi7-pixel <0que.pasaria.si7@gmail.com>
2025-11-15 02:04:44 -0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -3,27 +3,23 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import AdminPanel from './components/AdminPanel.tsx';
 
-const urlParams = new URLSearchParams(window.location.search);
-const isAdminRoute = urlParams.get('admin') === 'true';
+const RootSelector: React.FC = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const isAdminRoute = urlParams.get('admin') === 'true';
 
-if (isAdminRoute) {
-  const adminRootElement = document.getElementById('admin-root');
-  if (adminRootElement) {
-    const root = createRoot(adminRootElement);
-    root.render(
-      <React.StrictMode>
-        <AdminPanel />
-      </React.StrictMode>
-    );
+  if (isAdminRoute) {
+    return <AdminPanel />;
   }
-} else {
-  const rootElement = document.getElementById('root');
-  if (rootElement) {
-    const root = createRoot(rootElement);
-    root.render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    );
-  }
+  
+  return <App />;
+};
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <RootSelector />
+    </React.StrictMode>
+  );
 }
